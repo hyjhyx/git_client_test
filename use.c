e61d28ec91d73322d826d1728d7503e60138aca4
hyj <1677380905@qq.com>
2025-03-03 12:53:31 +0800

    最新

--- use.c
+++ use.c
@@ -3,10 +3,6 @@ using namespace std;
 
 int main(){
 
-
-	cout << "yes" << end;
-	cout << "I change this c file" << endl;
-	cout << "ks is learning" << endl;  # 新建了分支。
+	cout << "I want to be a post_graduate" << endl;`
 	return 0;
-
 }
